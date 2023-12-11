93c6f1663f1ebd1b52837cb8a169c8b560453df4
Alfred <somethingfly@somethingfly.com>
2023-12-10 19:41:12 -0800

    commit tree 038cf943ecdfb56fc79855c7623af83bd55dfbcd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  newArray = array.map.with_index do |e,i|
+    if (i == 0)
+      e
+    elsif (array.length == 2)
+      " and " + e
+    elsif (i < array.length - 1)
+      ", " + e
+    else
+      ", and " + e
+    end
+  end
+  newArray.join
 end
